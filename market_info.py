36e21f22bb5d2eff7ccdc3cc51cc47acd83e959e
oscjaimes <ojaimes@ualberta.ca>
2020-11-25 21:01:32 -0700

    found new links for getting csv files. testing on heroku

--- market_info.py
+++ market_info.py
@@ -34,23 +34,16 @@ def get_sector_list():
     return list(set(good_sectors))
 
 
-def get_nasdaq_csv():
-    headers = {
-        'authority': 'old.nasdaq.com',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'cross-site',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://github.com/shilewenuw/get_all_tickers/issues/2',
-        'accept-language': 'en-US,en;q=0.9',
-        'cookie': 'AKA_A2=A; NSC_W.TJUFEFGFOEFS.OBTEBR.443=ffffffffc3a0f70e45525d5f4f58455e445a4a42378b',
-    }
-
+def get_nyse_csv():
     r = requests.get(
-        'https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download', headers=headers)
+        'https://pkgstore.datahub.io/core/nyse-other-listings/nyse-listed_csv/data/3c88fab8ec158c3cd55145243fe5fcdf/nyse-listed_csv.csv')
+    data =io.StringIO(r.text)
+    df = pd.read_csv(data, sep=",")
+    print(df)
+
+def get_nasdaq_csv():
+    r = requests.get(
+        'https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed-symbols_csv/data/595a1f263719c09a8a0b4a64f17112c6/nasdaq-listed-symbols_csv.csv')
     data =io.StringIO(r.text)
     df = pd.read_csv(data, sep=",")
     print(df)
